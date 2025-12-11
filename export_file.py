f7312df735d42417c772c4db33ab1a0843082d64
Yaandle <131413645+Yaandle@users.noreply.github.com>
2025-12-11 14:17:56 +1100

    Refactor SQL query to simplify lap data retrieval
    Updated SQL query to fetch laps without row number and ordered by lap time.

--- export_file.py
+++ export_file.py
@@ -1,21 +1,17 @@
 import duckdb
 import pandas as pd
 
-csv_path = "     "      # Add / path to your data
+csv_path = "     "      # Add path to your data
 con = duckdb.connect()
 
-# Fetch all laps 
+# Fetch all laps without row_num
 query = f"""
-SELECT current_lap, row_num, gas, brake  
-FROM (
-    SELECT 
-        current_lap,
-        ROW_NUMBER() OVER (PARTITION BY current_lap ORDER BY lap_time_sec) AS row_num,
-        CAST(gas AS DOUBLE) AS gas,
-        CAST(brake AS DOUBLE) AS brake
-    FROM read_csv_auto('{csv_path}', header=True)
-)
-ORDER BY current_lap, row_num
+SELECT 
+    current_lap,
+    CAST(gas AS DOUBLE) AS gas,
+    CAST(brake AS DOUBLE) AS brake
+FROM read_csv_auto('{csv_path}', header=True)
+ORDER BY current_lap, lap_time_sec
 """
 df = con.execute(query).fetchdf()
 
